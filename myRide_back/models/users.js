ec8acdf9599525f1511f3b8f915788703e780365
Loussot <theo.loussot@gmail.com>
2023-02-21 18:03:43 +0100

    deploy backend vercel

--- myRide_back/models/users.js
+++ myRide_back/models/users.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose')
 const usersSchema = mongoose.Schema({
     email: String,
     password: String,
+    username:String,
+    token:String
 })
 
 const User = mongoose.model('users', usersSchema);
